2d5563777c1b40a6576b85bfa16d14db2ca3b087
Kevin Huang <yellow.khaos@gmail.com>
2019-02-03 02:20:01 +0000

    Automatically backed up by Learn

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -10,6 +10,13 @@ class Pancake extends React.Component {
     };
   }
 
+  componentDidMount() {
+    
+  }
+
+  componentWillUnmount() {
+    
+  }
   // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
 
   // TODO: create a componentWillUnmount() which will clear the interval
